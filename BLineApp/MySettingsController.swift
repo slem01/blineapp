aec4558900783dce365e1540864e69e27b48ea82
Shelby Lem <slem@nd.edu>
2018-04-09 09:38:25 -0400

    initial commit

--- BLineApp/MySettingsController.swift
+++ BLineApp/MySettingsController.swift
@@ -1,24 +1,27 @@
 //
-//  ViewController.swift
+//  MySettingsController.swift
 //  BLineApp
 //
-//  Created by sally everlove on 4/8/18.
+//  Created by sally everlove on 4/9/18.
 //  Copyright © 2018 Shelby Lem. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class MySettingsController: UIViewController {
+    //@IBOutlet weak var BackButton: UIBarButtonItem!
+    //BackButton = self.navigationItem.backBarButtonItem;
+    @IBOutlet weak var MySettingsNavigationBar: UINavigationItem!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
 }
-
